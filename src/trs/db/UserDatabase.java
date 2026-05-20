71b5fd142ee9360cec23f6bceb01091ac30954b7
Jenya <borisjenya2@gmail.com>
2026-05-20 20:32:40 +0300

    feat: implement UserDatabase with default users

---
+++ src/trs/db/UserDatabase.java
@@ -0,0 +1,37 @@
+package trs.db;
+
+import trs.entity.user.User;
+import trs.entity.user.UserRole;
+import trs.exception.DatabaseException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDatabase {
+
+    private static UserDatabase instance;
+    private Map<String, User> userBase;
+
+    private UserDatabase() {
+        this.userBase = new HashMap<>();
+        userBase.put("admin",new User("admin", "admin", UserRole.ADMIN));
+        userBase.put("firstuser", new User("firstuser", "firstuser", UserRole.USER));
+    }
+
+    public static UserDatabase getInstance(){
+
+        if (instance == null){
+            instance = new UserDatabase();
+        }
+        return instance;
+    }
+
+    public void registerUser(User user) throws DatabaseException{
+        if (userBase.containsKey(user.getLogin())) throw new DatabaseException("Користувач вже зареєстрований!");
+        userBase.put(user.getLogin(), user);
+    }
+
+    public User getUser(String login){
+        return userBase.get(login);
+    }
+}
