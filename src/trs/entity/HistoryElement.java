17cf75add2ddc28f8da25a681c2b68a3242cc995
Jenya <borisjenya2@gmail.com>
2026-05-21 20:43:59 +0300

    feat: add field String addInfo. add second creator with all fields parameters. update toString method

--- src/trs/entity/HistoryElement.java
+++ src/trs/entity/HistoryElement.java
@@ -9,17 +9,25 @@ public class HistoryElement {
 
     private LocalDateTime date;
     private ActionEvent action;
+    private String addInfo;
 
 
     public HistoryElement(LocalDateTime date, ActionEvent action) {
         this.date = date;
         this.action = action;
+        this.addInfo = "";
+    }
+
+    public HistoryElement(LocalDateTime date, ActionEvent action, String addInfo) {
+        this.date = date;
+        this.action = action;
+        this.addInfo = addInfo;
     }
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
         String finalDate = this.date.format(formatter);
-        return String.format("Дата: %s. Опис події: %s", finalDate, this.action.getDescription());
+        return String.format("Дата: %s. Опис події: %s %s", finalDate, this.action.getDescription(), this.addInfo);
     }
 }
