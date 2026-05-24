775795e98c8c64429e93559c63e2b557adabffaa
Jenya <borisjenya2@gmail.com>
2026-05-25 00:00:36 +0300

    feat: add method preregistrateVehicle, add business-logic to next methods: preregistrateVehicle, registrateVehicle, userMenu

--- src/trs/view/ConsoleMenu.java
+++ src/trs/view/ConsoleMenu.java
@@ -1,6 +1,13 @@
 package trs.view;
 
 import trs.controller.RegistrationManager;
+import trs.dto.CarDto;
+import trs.dto.MotoDto;
+import trs.dto.TruckDto;
+import trs.dto.VehicleDto;
+import trs.entity.Vehicle;
+import trs.entity.enums.BodyType;
+import trs.entity.enums.MotoType;
 import trs.entity.user.User;
 import trs.entity.user.UserRole;
 import trs.exception.AuthorizationException;
@@ -12,6 +19,9 @@ public class ConsoleMenu {
 
     private static Scanner scanner = new Scanner(System.in);
     private static RegistrationManager manager = RegistrationManager.getInstance();
+    private static final String VEHICLE_TYPES = "1 - Легковий автомобіль\n" +
+                                                "2 - Вантажний автомобіль\n" +
+                                                "3 - Мотоцикл";
 
     public void start(){
 
@@ -100,7 +110,217 @@ public class ConsoleMenu {
             System.out.println("5. Оновити власника ТЗ");
             System.out.println("6. Поточні завдання");
             System.out.println("Введіть номер пункту меню або натисніть 0 для виходу з системи: ");
+
+            int action;
+
+            try{
+                action = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (action){
+
+                    case 0:
+                        return;
+                    case 1:
+                        registrateOwner();
+                        break;
+                    case 2:
+                        updateOwnerNumber();
+                        break;
+                    case 3:
+                        deleteOwner();
+                        break;
+                    case 4:
+                        preregistrateVehicle();
+                        break;
+                    case 5:
+                        updateVehicleOwner();
+                        break;
+                    case 6:
+                    default:
+                        throw new IllegalArgumentException("Неіснуючий пункт меню, спробуйте ще раз!");
+                }
+
+
+            }catch (InputMismatchException e){
+                System.err.println("Некоректне значення, введіть число!");
+                scanner.nextLine();
+            } catch (IllegalArgumentException e){
+                System.err.println(e.getMessage());
+            }
         }
+    }
+
+
+    void registrateOwner(){
+
+
+    }
+
+    void updateOwnerNumber(){
+
+    }
+
+    void deleteOwner(){
+
+    }
+
+    void preregistrateVehicle(){
+
+        System.out.println("Введіть номер телефону власника: ");
+        String ownerPhone = scanner.nextLine();
+        if (!manager.isOwnerRegistred(ownerPhone)){
+            System.err.println("Власника за вказаним номером не знайдено! Додайте власника та спробуйте знову");
+            return;
+        }
+
+        System.out.printf("Оберіть тип транспортного засобу:%n%s%n", VEHICLE_TYPES);
+        System.out.println("Введіть значення:");
+        int type;
+
+        try{
+            type = scanner.nextInt();
+
+            switch (type){
+
+                case 1:
+                case 2:
+                case 3:
+                    scanner.nextLine();
+                    registrateVehicle(type, ownerPhone);
+                    return;
+                default:
+                    throw new IllegalArgumentException("Неіснуючий тип, спробуйте ще раз!");
+            }
+
+        }catch (InputMismatchException e){
+            System.err.println("Некоректне значення, введіть число!");
+            scanner.nextLine();
+        }
+    }
+
+    void registrateVehicle(int type, String ownerPhone) {
+
+        String vin;
+        String engineCode;
+        String color;
+        String model;
+
+        System.out.println("Введіть VIN-код: ");
+        vin = scanner.nextLine().trim();
+        System.out.println("Введіть код двигуна: ");
+        engineCode = scanner.nextLine().trim();
+        System.out.println("Введіть колір: ");
+        color = scanner.nextLine().trim();
+        System.out.println("Введіть марку та модель: ");
+        model = scanner.nextLine();
+
+        switch (type) {
+
+            case 1:
+
+                System.out.printf("Вкажіть тип кузову:%n%s", BodyType.getTypes());
+                int body;
+                while (true) {
+                    try {
+                        body = scanner.nextInt();
+                        if (body < 1 || body > BodyType.values().length)
+                            throw new IllegalArgumentException("Неіснуюче значення, спробуйте ще раз: ");
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.err.println("Некоректне значення, введіть число: ");
+                        scanner.nextLine();
+                    } catch (IllegalArgumentException e) {
+                        System.err.println(e.getMessage());
+                    }
+                }
+
+
+                CarDto carDto = new CarDto(vin, model, ownerPhone);
+                carDto.setEngineCode(engineCode);
+                carDto.setColor(color);
+                BodyType bodyType = BodyType.values()[body - 1];
+                carDto.setBodyType(bodyType);
+
+                manager.registerVehicle(carDto);
+                break;
+
+            case 2:
+
+                System.out.println("Введіть значення максимального навантаження(формат 22,9): ");
+                double loadCap;
+                while (true){
+                    try{
+                        loadCap = scanner.nextDouble();
+                        break;
+                    } catch (InputMismatchException e){
+                        System.err.println("Некоректне значення, введіть число:");
+                        scanner.nextLine();
+                    }
+                }
+
+                TruckDto truckDto = new TruckDto(vin, model, ownerPhone);
+                truckDto.setEngineCode(engineCode);
+                truckDto.setColor(color);
+                truckDto.setLoadCapacity(loadCap);
+
+                manager.registerVehicle(truckDto);
+                break;
+
+            case 3:
+
+                System.out.printf("Оберіть тип мотоцикла: %n%s%n", MotoType.getTypes());
+                int motoType;
+                while (true){
+                    try {
+                        motoType = scanner.nextInt();
+                        scanner.nextLine();
+                        if (motoType < 1 || motoType > MotoType.values().length){
+                            throw new IllegalArgumentException("Неіснуюче значення, спробуйте ще раз:");
+                        }
+                        break;
+                    } catch (InputMismatchException e){
+                        System.err.println("Некоректне значення, введіть число: ");
+                        scanner.nextLine();
+                    } catch (IllegalArgumentException e){
+                        System.err.println(e.getMessage());
+                        scanner.nextLine();
+                    }
+                }
+
+
+                System.out.println("Чи обладнаний мотоцикл боковою коляскою(+,-): ");
+                String sidecar;
+                boolean hasSidecar;
+                while (true){
+                    sidecar = scanner.nextLine();
+                    scanner.nextLine();
+                    if (sidecar.equals("-")){
+                        hasSidecar = false;
+                        break;
+                    } else if (sidecar.equals("+")) {
+                        hasSidecar = true;
+                        break;
+                    } else {
+                        System.err.println("Невірний формат! Введіть - або +:");
+                        scanner.nextLine();
+                    }
+                }
+
+                MotoDto motoDto = new MotoDto(vin, model, ownerPhone);
+                motoDto.setEngineCode(engineCode);
+                motoDto.setHasSidecar(hasSidecar);
+                motoDto.setColor(color);
+                motoDto.setType(MotoType.values()[motoType]);
+
+                manager.registerVehicle(motoDto);
+                break;
+
+        }
+    }
+
+    void updateVehicleOwner(){
+
     }
 
 }
