05d99966309ef0e507516d36d0e7f0d96fa1b12f
Jenya <borisjenya2@gmail.com>
2026-05-24 23:58:00 +0300

    feat: add business-logic to next methods: isOwnerRegistered, registerVehicle

--- src/trs/controller/RegistrationManager.java
+++ src/trs/controller/RegistrationManager.java
@@ -3,12 +3,22 @@ package trs.controller;
 import trs.db.OwnerDatabase;
 import trs.db.UserDatabase;
 import trs.db.VehicleDatabase;
+import trs.dto.CarDto;
+import trs.dto.MotoDto;
+import trs.dto.TruckDto;
 import trs.dto.VehicleDto;
 import trs.entity.Owner;
+import trs.entity.Vehicle;
+import trs.entity.enums.ActionEvent;
+import trs.entity.enums.VehicleStatus;
 import trs.entity.user.User;
 import trs.entity.user.UserRole;
 import trs.exception.AuthorizationException;
 import trs.exception.DatabaseException;
+import trs.factory.CarFactory;
+import trs.factory.MotoFactory;
+import trs.factory.TruckFactory;
+import trs.factory.VehicleFactory;
 
 
 public class RegistrationManager {
@@ -17,6 +27,8 @@ public class RegistrationManager {
     private final VehicleDatabase vehicleBase = VehicleDatabase.getInstance();
     private final OwnerDatabase ownerBase = OwnerDatabase.getInstance();
     private final UserDatabase userBase = UserDatabase.getInstance();
+    private VehicleFactory factory;
+
 
     private RegistrationManager() {}
 
@@ -30,14 +42,40 @@ public class RegistrationManager {
 
     public void registerVehicle(VehicleDto dto){
 
+        Vehicle vehicle = null;
+        Owner owner = findOwner(dto.getOwnerPhone());
+        if (dto instanceof CarDto){
+            factory = new CarFactory();
+            vehicle = factory.createVehicle(dto, owner);
+            factory = null;
+        }else if (dto instanceof TruckDto){
+            factory = new TruckFactory();
+            vehicle = factory.createVehicle(dto, owner);
+            factory = null;
+        } else if (dto instanceof MotoDto) {
+            factory = new MotoFactory();
+            vehicle = factory.createVehicle(dto, owner);
+            factory = null;
+        }
+
+        vehicle.addHistory(ActionEvent.ADDED_TO_SYSTEM);
+        vehicle.updateStatus(VehicleStatus.WAITING_FOR_REG_NUMBER);
+        vehicle.addHistory(ActionEvent.STATUS_CHANGED);
+
+        System.out.println("Успішно!!!!!!! " + vehicle.getVinCode());
+
     }
 
-    public void registerOwner(Owner owner){
-
+    public void registerOwner(String name, String surname, String phone, String email){
+        ownerBase.addOwner(new Owner(name, surname, phone, email));
     }
 
     public Owner findOwner(String phoneNum){
         return null;
+    }
+
+    public boolean isOwnerRegistred(String phoneNum){
+        return ownerBase.findByPhone(phoneNum) != null;
     }
 
     public void updateOwnerPhone(String current, String newPhone){
