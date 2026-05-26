780b2e6be8f293be1c01cc0bfed979b985a5b17d
Jenya <borisjenya2@gmail.com>
2026-05-26 23:12:21 +0300

    feat: add new enums TECHNICAL_INSPECTION and VIOLATION

--- src/trs/entity/enums/ActionEvent.java
+++ src/trs/entity/enums/ActionEvent.java
@@ -4,7 +4,9 @@ public enum ActionEvent {
     ADDED_TO_SYSTEM("Транспортний засіб зареєстровано в системі"),
     ASSIGNED_REG_NUMBER("Транспортному засобу присвоєно державний номер"),
     OWNER_CHANGED("Змінено власника транспортного засобу"),
-    STATUS_CHANGED("Статус транспортного засобу змінено");
+    STATUS_CHANGED("Статус транспортного засобу змінено"),
+    TECHNICAL_INSPECTION("Технічний огляд транспортного засобу"),
+    VIOLATION("Скоєно правопорушення");
 
     private String description;
 
