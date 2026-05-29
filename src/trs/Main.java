c4491295ad4ca111ec98985e61d548b4637a574a
Jenya <borisjenya2@gmail.com>
2026-05-29 08:07:45 +0300

    feat: dependency injection AdminStatisctic --> ConsoleMenu

--- src/trs/Main.java
+++ src/trs/Main.java
@@ -1,10 +1,16 @@
 package trs;
 
+import trs.controller.RegistrationManager;
+import trs.statistic.AdminStatistic;
 import trs.view.ConsoleMenu;
 
 public class Main {
     public static void main(String[] args) {
-        ConsoleMenu menu = new ConsoleMenu();
+        RegistrationManager manager = RegistrationManager.getInstance();
+        AdminStatistic statistic = new AdminStatistic();
+        manager.addSubscriber(statistic);
+
+        ConsoleMenu menu = new ConsoleMenu(statistic);
         menu.start();
     }
 }
