eaa008eec0ee226619b9fbbcdfef8600eb40f844
Jenya <borisjenya2@gmail.com>
2026-05-29 07:52:59 +0300

    feat: add and implement method isGovNumberValid

--- src/trs/controller/Validator.java
+++ src/trs/controller/Validator.java
@@ -3,7 +3,7 @@ package trs.controller;
 public class Validator {
 
     private static final int VIN_LENGTH = 10;
-    private static final int ENG_CODE_LENGTH = 8;
+    private static final int ENG_CODE_GOVNUM_LENGTH = 8;
     private static final int PH_NUMBER_LENGTH = 12;
 
 
@@ -26,13 +26,13 @@ public class Validator {
     // Engine-code format XXX1234X
     public static boolean isEngineCodeValid(String engineCode){
 
-        if (engineCode.length() != ENG_CODE_LENGTH) return false;
+        if (engineCode.length() != ENG_CODE_GOVNUM_LENGTH) return false;
 
-        for (int i = 0; i < ENG_CODE_LENGTH; i++) {
-            if (i < 3 || i == ENG_CODE_LENGTH - 1){
+        for (int i = 0; i < ENG_CODE_GOVNUM_LENGTH; i++) {
+            if (i < 3 || i == ENG_CODE_GOVNUM_LENGTH - 1){
                 if (!Character.isLetter(engineCode.charAt(i))) return false;
             }
-            if (i > 2 && i < ENG_CODE_LENGTH - 1){
+            if (i > 2 && i < ENG_CODE_GOVNUM_LENGTH - 1){
                 if (!Character.isDigit(engineCode.charAt(i))) return false;
             }
         }
@@ -40,14 +40,30 @@ public class Validator {
     }
 
     // Phone-number format 380XXXXXXXXX
-    public static boolean isPhoneNumberValid(String number){
+    public static boolean isPhoneNumberValid(String phoneNumber){
 
-        if (number.length() != PH_NUMBER_LENGTH) return false;
+        if (phoneNumber.length() != PH_NUMBER_LENGTH) return false;
 
-        if (!number.startsWith("380")) return false;
+        if (!phoneNumber.startsWith("380")) return false;
 
         for (int i = 0; i < PH_NUMBER_LENGTH; i++) {
-            if (!Character.isDigit(number.charAt(i))) return false;
+            if (!Character.isDigit(phoneNumber.charAt(i))) return false;
+        }
+        return true;
+    }
+
+    // Govnumber format XX1234XX
+    public static boolean isGovNumberValid(String govNumber){
+
+        if (govNumber.length() != ENG_CODE_GOVNUM_LENGTH) return false;
+
+        for (int i = 0; i < ENG_CODE_GOVNUM_LENGTH; i++) {
+            if (i < 2 || i > 5){
+                if (!Character.isLetter(govNumber.charAt(i))) return false;
+            }
+            if (i > 1 && i < 6){
+                if (!Character.isDigit(govNumber.charAt(i))) return false;
+            }
         }
         return true;
     }
